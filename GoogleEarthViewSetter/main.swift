48146c8adbf2ca390f3da59dd2cfb689f8168967
robertazzopardi <robertazzopardi@icloud.com>
2020-06-18 15:13:04 +0100

    Initial Commit

---
+++ GoogleEarthViewSetter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GoogleEarthViewSetter
+//
+//  Created by Robert Azzopardi-Yashi on 18/06/2020.
+//  Copyright © 2020 BisonCo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
